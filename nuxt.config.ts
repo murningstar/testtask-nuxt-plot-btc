2a2a6da4aeb4bce566afd577adda706b6ac0ee58
menyadeniskostin <menyadeniskostin@gmail.com>
2023-12-08 07:59:09 +0300

    Naive-UI installations+preconfigurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,25 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ["@nuxtjs/tailwindcss"],
     tailwindcss: { configPath: "tailwind.config", exposeConfig: true },
+
+    /* Naiveui Pre-config */
+    build: {
+        transpile:
+            process.env.NODE_ENV === "production"
+                ? [
+                      "naive-ui",
+                      "vueuc",
+                      "@css-render/vue3-ssr",
+                      "@juggle/resize-observer",
+                  ]
+                : ["@juggle/resize-observer"],
+    },
+    vite: {
+        optimizeDeps: {
+            include:
+                process.env.NODE_ENV === "development"
+                    ? ["naive-ui", "vueuc", "date-fns-tz/formatInTimeZone"]
+                    : [],
+        },
+    },
 });
