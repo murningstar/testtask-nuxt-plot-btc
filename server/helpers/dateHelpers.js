67f7c57ad45d58bbf9a66f664d5e0a99453a4ed0
menyadeniskostin <menyadeniskostin@gmail.com>
2023-12-08 08:00:29 +0300

    +dateHelpers.js

---
+++ server/helpers/dateHelpers.js
@@ -0,0 +1,20 @@
+const nowDate = new Date();
+function getDateDayAgo() {
+    const res = new Date().setDate(nowDate.getDate() - 1);
+    return new Date(res);
+}
+function getDateWeekAgo() {
+    const res = new Date().setDate(nowDate.getDate() - 7);
+    return new Date(res);
+}
+
+function getDateMonthAgo() {
+    const res = new Date().setMonth(nowDate.getMonth() - 1);
+    return new Date(res);
+}
+function getDateYearAgo() {
+    const res = new Date().setFullYear(nowDate.getFullYear() - 1);
+    return new Date(res);
+}
+export { getDateDayAgo, getDateWeekAgo, getDateMonthAgo, getDateYearAgo };
+console.log(getDateDayAgo());
